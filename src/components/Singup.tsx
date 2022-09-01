2a294a4ddfa0df827df90000165ff46ec3c4d3da
DevWonny <cjfdnjs1994@naver.com>
2022-09-01 14:02:51 +0900

    feat: 간단한 회원가입 Form 구현

--- src/components/Singup.tsx
+++ src/components/Singup.tsx
@@ -9,10 +9,28 @@ interface LabelValue {
 }
 
 const SignUp = () => {
-  // label 위치
-  const [labelPosition, setLabelPosition] = useState(false);
+  // Name Label Position
+  const [nameLabelPosition, setNameLabelPosition] = useState(false);
+  // ID Label Position
+  const [idLabelPosition, setIdLabelPosition] = useState(false);
+  // Password Label Position
+  const [passwordLabelPosition, setPasswordLabelPosition] = useState(false);
+  // Password Confirm Label Position
+  const [passwordConfirmLabelPosition, setPasswordConfirmLabelPosition] =
+    useState(false);
+  // Email Label Position
+  const [emailLabelPosition, setEmailLabelPosition] = useState(false);
+
   // name
   const [name, setName] = useState("");
+  // Id
+  const [id, setId] = useState("");
+  // password
+  const [password, setPassword] = useState("");
+  // password Confirm
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  // email
+  const [email, setEmail] = useState("");
 
   return (
     <Container>
@@ -23,19 +41,85 @@ const SignUp = () => {
             type='text'
             id='name'
             required
-            onFocus={() => setLabelPosition(true)}
-            onBlur={() => setLabelPosition(false)}
+            onFocus={() => setNameLabelPosition(true)}
+            onBlur={() => setNameLabelPosition(false)}
             value={name}
             onChange={(e) => setName(e.target.value)}
           />
-          <Label htmlFor='name' isFocus={labelPosition} nameValue={name}>
+          <Label htmlFor='name' isFocus={nameLabelPosition} nameValue={name}>
             Name
           </Label>
         </InputContainer>
+
         {/* ID */}
+        <InputContainer>
+          <Input
+            type='text'
+            id='id'
+            required
+            onFocus={() => setIdLabelPosition(true)}
+            onBlur={() => setIdLabelPosition(false)}
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+          />
+          <Label htmlFor='id' isFocus={idLabelPosition} nameValue={id}>
+            ID
+          </Label>
+        </InputContainer>
+
         {/* Password */}
+        <InputContainer>
+          <Input
+            type='text'
+            id='password'
+            required
+            onFocus={() => setPasswordLabelPosition(true)}
+            onBlur={() => setPasswordLabelPosition(false)}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Label
+            htmlFor='password'
+            isFocus={passwordLabelPosition}
+            nameValue={password}>
+            Password
+          </Label>
+        </InputContainer>
+
         {/* Password Confirm */}
+        <InputContainer>
+          <Input
+            type='text'
+            id='passwordConfirm'
+            required
+            onFocus={() => setPasswordConfirmLabelPosition(true)}
+            onBlur={() => setPasswordConfirmLabelPosition(false)}
+            value={passwordConfirm}
+            onChange={(e) => setPasswordConfirm(e.target.value)}
+          />
+          <Label
+            htmlFor='passwordConfirm'
+            isFocus={passwordConfirmLabelPosition}
+            nameValue={passwordConfirm}>
+            Password Confirm
+          </Label>
+        </InputContainer>
+
         {/* Email */}
+        <InputContainer>
+          <Input
+            type='text'
+            id='email'
+            required
+            onFocus={() => setEmailLabelPosition(true)}
+            onBlur={() => setEmailLabelPosition(false)}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Label htmlFor='email' isFocus={emailLabelPosition} nameValue={email}>
+            Email
+          </Label>
+        </InputContainer>
       </SignUpWrap>
     </Container>
   );
@@ -59,28 +143,34 @@ const SignUpWrap = styled.div`
 const InputContainer = styled.div`
   position: relative;
   width: 100%;
-
-  /* display: flex;
-  align-items: center; */
+  height: 80px;
+  padding-left: 20px;
+  margin-bottom: 20px;
 `;
 
 const Input = styled.input`
-  padding: 10px;
-  border: none;
-  border-radius: 4px;
+  position: absolute;
+  bottom: 0;
+  width: 70%;
+  height: 50px;
+  border: 1px solid #000;
+  border-radius: 10px;
+  box-sizing: border-box;
   font: inherit;
-  outline: 2px solid #000;
+  outline: none;
 `;
 
-// typesciprt styled components props setting
+// typescript styled components props setting
 // 단일 props 사용시
 // const Label = styled.label < {propsName : Type} >``
 const Label = styled.label<LabelValue>`
   position: absolute;
-  top: 0;
-  left: 0;
+  width: 70%;
+  bottom: 15px;
+  left: 30px;
+  font-size: 18px;
   transition: transform 0.25s;
-  color: ${(props) => (props.isFocus ? "red" : "purple")};
+  color: ${(props) => (props.isFocus ? "red" : "#2e2e2e")};
   transform: ${(props) =>
-    props.isFocus || props.nameValue ? "translate(0,-30px)" : "translate(0,0)"};
+    props.isFocus || props.nameValue ? "translate(0,-40px)" : "translate(0,0)"};
 `;
